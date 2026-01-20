6f5797fe07eff44b03826e67338633347ded40c3
Reisya Okan <reisyaokan24@gmail.com>
2026-01-20 12:05:36 +0700

    learn iterate, add, remove, modify, and sorting item to the list

--- list.py
+++ list.py
@@ -38,17 +38,129 @@
     When [..:..:..] --> Accessing all the items from index number zero to the last index number
          [..:5 :..] --> Accessing from index number zero to 5 because stop in 5 index
          [1 :..:..] --> Accessing from 1 index to the last index number
-         [..:..:-1] --> Accesing reverse all items 
+         [..:..:-1] --> Accesing reverse all items
+
+    How to iterate to the list
+    Syntax:
+    for ... in variable:
+        print(...):
+    How to add items to the list
+    1. Append --> add item to the end of the list
+       Syntax:
+       variable.append(..)
+    2. Insert --> insert item any position
+       Syntax:
+       variable.insert(index number, object)
+    3. Extend --> add another list(sublist)
+       Join two list
+       Example:
+       [1,2,3,4,5] [6,7,8,9,10] --> Join two list
+       Syntax:
+       variable.extend(iterable)
+    How to remove the item
+    1. Remove
+       Syntax:
+       variable.remove(value)
+    2. Pop
+       Syntax:
+       variable.pop(index number)
+    3. Clear
+       Clear or remove all item, When remove all the items, it will be empty list
+       Syntax:
+       variable.clear()
+    4. Del statement
+       remove and deletes the structure of the list
+       Syntax:
+       del variable 
+    How to modify a single item
+    Syntax:
+    variable[index number] = new items
+    How to modify a range of item by using slicing
+    Create a new variable then fill it with a new item, then use the syntax as below
+    new variable = [new item]
+    old variable[start index number:stop index number] = new variable 
+    
+    Sorting
+    Sorting means arranging items in order, sorting can bedone in two ways:
+    1. Ascending order --> Low number to high
+    2. Descending order --> High number to low
+    Syntax:
+    variable.sort() --> Ascending order
+    variable.sort(reverse = True) --> Descending order
+
+    Maximum number
+    Syntax:
+    max(variable)
+    Minimum number
+    Syntax:
+    min(variable)
+    Total number
+    Syntax:
+    sum(variable)
 '''
 
 myList1 = [10, 20, 30, 40 ,50]
-mylist2 = list([100, 'Hello', 50.50, True])
+myList2 = [60, 70, 80, 90 ,100]
+mylist3 = list([100, 'Hello', 50.50, True])
 print(myList1)
-print(myList1[1])
-print(myList1[-1])
+
+# Function len
 print(len(myList1))
 print(myList1[len(myList1)-1])
-print(mylist2)
+print(myList2)
+
+# Index Number
+print(myList1[1])
+print(myList1[-1])
 idx = myList1.index(40, 2)
 print(idx)
 print(myList1[2:4])
+
+# Iterate to the list
+for item in myList1:
+    print(item)
+
+# Add item to the list:
+myList1.append(60)
+myList1.insert(2, 25)
+print(myList1)
+
+# Join two list
+myList1.extend(myList2)
+print(myList1)
+
+# Remove item
+myList1.remove(60)
+print(myList1)
+myList1.pop(2)
+print(myList1)
+#myList1.clear()
+#print(myList1)
+
+# Modify Single item
+myList1[2] = 25
+print(myList1)
+
+# Modify range of items 
+l1 = [100, 200, 250]
+myList1[0:2] = l1 
+print(myList1)
+
+# Sorting
+myList4 = [6, 2, 4, 3, 1, 5]
+myList4.sort() 
+print(myList4)
+myList4.sort(reverse=True)
+print(myList4)
+
+# Maximum 
+mx = max(myList4)
+print(mx)
+
+# Minimum
+mn = min(myList4)
+print(mn)
+
+# Total
+total = sum(myList4)
+print(total)
