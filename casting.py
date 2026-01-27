557a72e655d8c04478610e6efc5e82020731a221
Reisya Okan <reisyaokan24@gmail.com>
2026-01-27 13:11:47 +0700

    learn to type casting and type conversion in python

--- casting.py
+++ casting.py
@@ -1,3 +1,27 @@
+'''
+    Type Casting and Type Conversion
+
+    Types of Casting
+    1. Implicit = convert automatically
+    2. Explicit = by using function
+
+    int() = will convert to int data from any data type
+    float() = will convert to float data from any data type
+    str() = will convert to string data from any data type
+    bool() = will conveert to boolean data from any data type
+
+    input() = taking inputs from user,  entering values dynamically keyboard
+    Example:
+    x = float(input("Please Enter Number 1: "))
+    y = float(input("Please Enter Number 2: "))
+    sum = x + y
+    print(sum)
+
+    range() = will generate numbers, syntax --> range(start, stop, step)
+    Example:
+    for x in range(1, 21, 1):
+        print(x, end=" ")
+'''
 # Casting adalah merubah dari satu tipe data ke tipe lain
 # Berikut cara casting
 
@@ -36,3 +60,13 @@ data_float = float(data)
 print("Data = ", data_integer, type(data_integer))
 print("Data = ", data_boolean, type(data_boolean))
 print("Data = ", data_float, type(data_float))
+
+# Input 
+x = float(input("Please Enter Number 1: "))
+y = float(input("Please Enter Number 2: "))
+sum = x + y
+print(sum)
+
+# Range
+for x in range(1, 21, 2):
+    print(x, end=" ")
